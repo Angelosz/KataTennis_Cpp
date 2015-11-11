6ba9ee87aee2d9469a198b0476ae47dc1bccbdb8
Angel <angelcabgil@gmail.com>
2015-11-11 00:31:06 +0000

    PointsAdder added

---
+++ Source/PointsAdder.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Player.h"
+
+class PointsAdder
+{
+public:
+
+	void addPointsToPlayerScore(Player& Player, int times)
+	{
+		for (int i = 0; i < times; i++)
+			Player.addPoint();
+	}
+};
