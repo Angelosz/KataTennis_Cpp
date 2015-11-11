1d009cc753b6797813122fd4dd2a2e630d8b4e63
Angel <angelcabgil@gmail.com>
2015-11-11 00:31:58 +0000

    Refactor: Using Player Instead of PlayerScore

--- Test/PlayerScoreTest/PlayerScoreShould.cpp
+++ Test/PlayerScoreTest/PlayerScoreShould.cpp
@@ -1,46 +1,43 @@
 #include "../TestRunner/catch.hpp"
-#include <PlayerScore.h>
-
+#include <Player.h>
+#include <PointsAdder.h>
 /*
 * 1. Each player can have either of these points in one game 0 15 30 40
 */
 
-void addPointsToPlayerScore(PlayerScore& PlayerScore, int times)
-{
-	for (int i = 0; i < times; i++)
-		PlayerScore.addPoint();
-}
+
 
 TEST_CASE("PlayerScore should")
 {
-	PlayerScore PlayerScore;
+	Player Player("Player");
+	PointsAdder PointsAdder;
 
 	SECTION("have 15 points after winning one ball")
 	{
-		PlayerScore.addPoint();
+		Player.addPoint();
 
-		REQUIRE(PlayerScore.getPoints() == 15);
+		REQUIRE(Player.getPoints() == 15);
 	}
 
 	SECTION("have 30 points after winning two balls")
 	{
-		addPointsToPlayerScore(PlayerScore, 2);
+		PointsAdder.addPointsToPlayerScore(Player, 2);
 
-		REQUIRE(PlayerScore.getPoints() == 30);
+		REQUIRE(Player.getPoints() == 30);
 	}
 
 	SECTION("have 40 points after winning three balls")
 	{
-		addPointsToPlayerScore(PlayerScore, 3);
+		PointsAdder.addPointsToPlayerScore(Player, 3);
 
-		REQUIRE(PlayerScore.getPoints() == 40);
+		REQUIRE(Player.getPoints() == 40);
 	}
 
-	SECTION("have 40 points and an advantage after winning four balls")
+	SECTION("have 40 points and one advantage after winning four balls")
 	{
-		addPointsToPlayerScore(PlayerScore, 4);
+		PointsAdder.addPointsToPlayerScore(Player, 4);
 
-		REQUIRE(PlayerScore.getPoints() == 40);
-		REQUIRE(PlayerScore.getAdvantage() == 1);
+		REQUIRE(Player.getPoints() == 40);
+		REQUIRE(Player.getAdvantage() == 1);
 	}
 }
